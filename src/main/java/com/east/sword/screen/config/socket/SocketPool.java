8e6b4e7a58200c47a163ce05b0cfa3e196a21c58
zhengzhendong <zzd@163.com>
2020-03-18 08:28:36 +0800

    socket

---
+++ src/main/java/com/east/sword/screen/config/socket/SocketPool.java
@@ -0,0 +1,23 @@
+package com.east.sword.screen.config.socket;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @CreateDate 8:19 2020/3/18.
+ * @Author ZZD
+ */
+public class SocketPool {
+
+    private static final ConcurrentHashMap<String, ClientSocket> ONLINE_SOCKET_MAP = new ConcurrentHashMap<>();
+
+
+    public static void add(ClientSocket clientSocket){
+        if (clientSocket != null && !clientSocket.getKey().isEmpty())
+            ONLINE_SOCKET_MAP.put(clientSocket.getKey(), clientSocket);
+    }
+
+    public static void remove(String key){
+        if (!key.isEmpty())
+            ONLINE_SOCKET_MAP.remove(key);
+    }
+}
