88381113be2fa3b7688f792d8a6c2d42389b85b4
zhengzhendong <zzd@163.com>
2020-03-24 10:13:06 +0800

    金晓通信--最新

--- src/main/java/com/east/sword/screen/util/socket/SocketTest.java
+++ src/main/java/com/east/sword/screen/util/socket/SocketTest.java
@@ -1,5 +1,6 @@
 package com.east.sword.screen.util.socket;
 
+import com.east.sword.screen.util.HexHelp;
 import com.east.sword.screen.vo.GoldMon;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,44 +18,29 @@ public class SocketTest {
         String host = "37.168.190.172";
         int port = 60001;
 
+        byte[] bb = {0x00, 0x00, 0x39, 0x37};
+        int cc = HexHelp.calCRC(bb);
+        System.out.println("校验crc正确性:63929---" + cc);
 
 
         //String msgHex = "02 00 00 39 37 f9 12 03";
         //0200003030303030373030303130303030365c433036343038
         //0200003030303030373030303130303030365c433036343038
         File file = new File("d:/play1.lst");
-        List<String> frameBody = GoldMon.getFrameFileBody(file);
+        List<String> frameBodyList = GoldMon.getFrameFileBodyOrigin(file);
+
+        for(int i =0 ;i < frameBodyList.size(); i++) {
+            String frameBody = frameBodyList.get(i);
+            String msgHex = GoldMon.getSendMessageStr(file.getName(),GoldMon.FRAME_UPLOAD_FILE, frameBody,i);
+            System.out.println("完整帧：" + msgHex);
+            byte[] msgByte = HexHelp.hexStringToBytes(msgHex);
+            String msg = SocketSender.getInstance().sendMessage(host, port, msgByte);
+            System.out.println("返回值："+msg);
+        }
 
         //System.out.println(frameBody.size());
-        String msgHex = GoldMon.getSendMessageStr(GoldMon.FRAME_UPLOAD_FILE,frameBody.get(0));
-        byte[] msgByte = hexStrToBinaryStr(msgHex);
-        String msg = SocketSender.getInstance().sendMessage(host,port,msgByte);
-        System.out.println(msg);
-       // byte[] response = {2, 0, 0, 49, 48, 48, -63, 83, 3};
+
+        // byte[] response = {2, 0, 0, 49, 48, 48, -63, 83, 3};
     }
 
-    public static byte[] hexStrToBinaryStr(String hexString) {
-        hexString = hexString.replaceAll(" ", "");
-        int len = hexString.length();
-        int index = 0;
-        byte[] bytes = new byte[len / 2];
-        while (index < len) {
-            String sub = hexString.substring(index, index + 2);
-            bytes[index/2] = (byte)Integer.parseInt(sub,16);
-            index += 2;
-        }
-        return bytes;
-    }
-
-    public static String bytesToHex(byte[] bytes) {
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < bytes.length; i++) {
-            String hex = Integer.toHexString(bytes[i] & 0xFF);
-            if(hex.length() < 2){
-                sb.append(0);
-            }
-            sb.append(hex);
-        }
-        return sb.toString();
-    }
 }
